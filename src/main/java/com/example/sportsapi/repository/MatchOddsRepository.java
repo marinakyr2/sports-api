5be03b0717d38f0965db772ff9047a7d6d0d1de1
marinakyr2 <marina.kyr2@gmail.com>
2024-12-15 17:44:58 +0200

    add component annotations

--- src/main/java/com/example/sportsapi/repository/MatchOddsRepository.java
+++ src/main/java/com/example/sportsapi/repository/MatchOddsRepository.java
@@ -2,6 +2,8 @@ package com.example.sportsapi.repository;
 
 import com.example.sportsapi.entity.MatchOdds;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MatchOddsRepository extends JpaRepository<MatchOdds, String> {
 }
--- src/main/java/com/example/sportsapi/repository/PopulateTables.java
+++ src/main/java/com/example/sportsapi/repository/PopulateTables.java
@@ -3,15 +3,22 @@ package com.example.sportsapi.repository;
 import com.example.sportsapi.entity.Match;
 import com.example.sportsapi.entity.enums.Sport;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@Component
 public class PopulateTables implements CommandLineRunner {
 
-    private MatchRepository matchRepository;
-    private MatchOddsRepository matchOddsRepository;
+    private final MatchRepository matchRepository;
+    private final MatchOddsRepository matchOddsRepository;
+
+    public PopulateTables(MatchRepository matchRepository, MatchOddsRepository matchOddsRepository) {
+        this.matchRepository = matchRepository;
+        this.matchOddsRepository = matchOddsRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
